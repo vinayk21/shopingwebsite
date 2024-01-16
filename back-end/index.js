da229c75f9035d74856a1b4aae1563b5465b9e36
vinayk21 <vickyahir8290@gmail.com>
2024-01-16 17:16:40 +0530

    adeed

---
+++ back-end/index.js
@@ -0,0 +1,8 @@
+const express =require("express");
+const app = express();
+const mongoose = require("mongoose");
+console.log("running");
+const data = async =>{
+    mongoose.connect("")
+}
+app.listen(5000);
--- front-end/src/Componets/Product.js
+++ front-end/src/Componets/Product.js
@@ -1,42 +1,97 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DataTable from 'react-data-table-component'
 const Product = () => {
-    const data = [
-        {
-            id: 1,
-            title: 'Beetlejuice',
-            year: '1988',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-    ]
-    const columns = [
-        {
-            name:`id`,
-            selector:row=>row.id,
-            sortable:true,
-        },
-        {
-            name: 'Title',
-            selector: row => row.title,
-            sortable:true,
+//     let dataa = [];
+// let obj = {};
+// let vvv = [1,2,3,4,5,6,7,8,9,9,0,876,543,323,4545,345,544];
+// // debugger
+// let rar = [];
+// let rr = vvv.filter((el)=>{
+//     // debugger
+//     if(el>5){
+//         console.log("ram",el);
+//      return el;
+//     }
+//     console.log("ajay",rar = [...rar,el]);
+// }).map((rrr)=>{
+//     // debugger    
+     
+//     if(rrr){
+//         return rrr * 10
+//     }
+// })
+// // debugger
+// rr.forEach((ttt)=>{
+//     debugger
+//     if(ttt){
+//        obj[ttt] = ttt;
+//     }
+// }
+// )
+// console.log("rr",obj);
+// useEffect(()=>{
+   
+//         indexx('completed',"val", 1)
+//         vinay("vina")
+    
+//     },[])
+//     const indexx = (status,key,date) =>{
+//         if(status = "completed"){
+//                 let iddd = dataa?.findIndex((ee)=>ee.val === date)
+//                 if(iddd > -1){
+//                     dataa[iddd][key] +=1;
             
-        },
-        {
-            name: 'Year',
-            selector: row => row.year,
-            sortable:true,
-        },
-    ];   
+//                 }else{
+//                     // const newobj = {...obj}
+//                     obj[key] =1;
+//                     // dataa.push(obj)
+//                     dataa = [...dataa,obj]
+//                 }
+//             }
+//         }
+//  const vinay = (hii) =>{ 
+//     return `heeelooo ${hii}`
+//  }
+// // indexx('completed',"val", 1)
+// // indexx('completed',"val", 1)
+// // console.log("obj",obj)
+// // console.log("data",dataa)
+//     const data = [
+//         {
+//             id: 1,
+//             title: 'Beetlejuice',
+//             year: '1988',
+//         },
+//         {
+//             id: 2,
+//             title: 'Ghostbusters',
+//             year: '1984',
+//         },
+//     ]
+//     const columns = [
+//         {
+//             name:`id`,
+//             selector:row=>row.id,
+//             sortable:true,
+//         },
+//         {
+//             name: 'Title',
+//             selector: row => row.title,
+//             sortable:true,
+            
+//         },
+//         {
+//             name: 'Year',
+//             selector: row => row.year,
+//             sortable:true,
+//         },
+//     ];   
     
    
     
   return (
     <>
-      <DataTable data={data} columns={columns}  selectableRows/>
+      {/* <DataTable data={data} columns={columns}  selectableRows style={{margin:"100px "}} /> */}
     </>
   )
 }
